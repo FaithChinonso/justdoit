4b8067f8078a84570d69bb3832471f8ff2c6242c
FaithChinonso <faithuchinonso@gmail.com>
2022-06-16 11:54:32 +0100

    local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import InputField from "./components/inputField";
 import TodoList from "./components/todoList";
 import { Todo } from "./model";
 
 const App: React.FC = () => {
+  let sessionTodos: Todo[];
+  const storedTodo: Todo[] = JSON.parse(sessionStorage.getItem("todo") || "");
+  if (storedTodo.length !== 0) {
+    sessionTodos = storedTodo;
+    console.log(storedTodo);
+  } else {
+    sessionTodos = [];
+  }
   const [todo, setTodo] = useState<string>("");
-  const [todos, setTodos] = useState<Todo[]>([]);
+  const [todos, setTodos] = useState<Todo[]>(sessionTodos);
   // const [completedTodos, setCompletedTodos] = useState<Todo[]>([]);
 
   const handleAdd = (e: React.FormEvent) => {
@@ -16,10 +24,14 @@ const App: React.FC = () => {
     }
     if (todos) {
       setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+
       setTodo("");
     }
     console.log(todos);
   };
+  useEffect(() => {
+    sessionStorage.setItem("todo", JSON.stringify(todos));
+  }, [todos]);
   return (
     <div className="App">
       <div className="heading">
